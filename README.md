e9184a8109aeee6450cac8f45be02813f013d21d
Katie Wang <34468317+KTWangster@users.noreply.github.com>
2018-02-21 17:27:02 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-A classic movie themed hang-man game using only HTML, CSS, and Javascript.
-
-Solve the word based on the theme before your guesses run out!
+# Hangman-Game
--- javascript/game.js
+++ javascript/game.js
@@ -10,11 +10,6 @@ var guessesMade = [];
 //Choose random word for player to guess from array!
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
-function play_single_sound() {
-    document.getElementById('audiotag1').play();
-}
-
-// eval(randomWord).getMusic();
 // Displays _ for all letters in word and gives them id by index
 function displayWord() {
 
@@ -25,7 +20,6 @@ function displayWord() {
         li.appendChild(document.createTextNode("_"));
         li.setAttribute("id", ("letter" + i));
         ul.appendChild(li);
-
     }
 }
 
@@ -35,10 +29,9 @@ function newWord() {
     displayWord();
     document.getElementById("listedLetters").innerHTML = "<li id='guess'> </li>";
 }
-
 displayWord();
 
-// Starts game
+// Function that starts game
 function startGame() {
 
     document.onkeyup = function (event) {
@@ -46,10 +39,9 @@ function startGame() {
         // Determines which key was pressed.
         var userGuess = event.key;
 
-        // Displays keys already pressed. 
+        // Function that displays keys already pressed. 
         function typedLetters() {
             var liList = document.getElementById("listedLetters").getElementsByTagName("li");
-
             var count = liList.length
             var countLoops = 0;
 
@@ -72,7 +64,7 @@ function startGame() {
             }
         }
 
-        //USING REGEX TO MAKE SURE THAT typedLetters() DOESN'T LOG NON REGULAR AND LOWERCASE LETTERS
+        // Regex to verify typedLetters() doesn't log regular or lowercase letters
         var pat = /^[a-z]+$/;
         if (pat.test(userGuess)) {
             typedLetters();
@@ -82,19 +74,19 @@ function startGame() {
         function checkList(word) {
 
             var wrongCounter = 0;
-            // For Loop iterates through word and checks if letter matches user guess.
+            // for Loop that iterates through word and checks if letter matches user guess.
             for (i = 0; i < word.length; i++) {
 
                 if (word[i] === userGuess) {
 
-                    //Changes _ to correct letter(s) guessed
+                    // for loop that changes _ to correct letter(s) guessed
                     for (i = 0; i < word.length; i++) {
                         if (word[i] === userGuess) {
                             var li = document.getElementById(("letter" + i)).innerHTML = word[i];
                         }
                     }
 
-                    //CHECK IF PLAYER WON STARTS HERE
+                    // Checks if user won
                     var saveWord = "";
 
                     //for loop to check player's progress on word
@@ -115,15 +107,15 @@ function startGame() {
                             "Losses: " + losses + "<br />" +
                             "Guesses Remaining: " + guessesRemaining;
                         document.querySelector("#scoreCounter").innerHTML = winCounter;
-                         
+
                         var gameOver = "You won the game! Press the enter key to play again!";
                         document.querySelector("#game").innerHTML = gameOver;
                         eval(randomWord);
 
                         guessesMade = [];
 
-                        // Pressing the enter key after winninng resets game.
-                        document.onkeyup = function(event) {
+                        // Pressing the enter key after winning resets game.
+                        document.onkeyup = function (event) {
                             if (event.which == 13 || event.keyCode == 13) {
                                 restartGame();
                                 startGame();
@@ -137,14 +129,14 @@ function startGame() {
 
                 if (wrongCounter === word.length) {
 
-                    //FOR LOOP TEST TO CHECK ARRAY FOR ALREADY GUESSED 
+                    // For loop to check array for already guessed
 
                     // Decrement 'Guesses Remaining' if guess is wrong.
                     var pat = /^[a-z]+$/;
                     if (pat.test(userGuess)) {
 
                         if (guessesMade.indexOf(userGuess) === -1) {
-                            guessesRemaining --;
+                            guessesRemaining--;
                             guessesMade.push(userGuess);
                         }
                     }
@@ -153,9 +145,9 @@ function startGame() {
                     var guessCounter = "Wins: " + wins + "<br />" +
                         "Losses: " + losses + "<br />" +
                         "Guesses Remaining: " + guessesRemaining;
-                    document.querySelector("#scoreCounnter").innerHTML = guessCounter;
+                    document.querySelector("#scoreCounter").innerHTML = guessCounter;
 
-                    var gameWrong = "Your guess is incorrect. You have " + guessesRemaining + " guesses remaing!";
+                    var gameWrong = "Your guess is incorrect. You have " + guessesRemaining + " guesses remaing.";
                     document.querySelector("#game").innerHTML = gameWrong;
                     if (guessesRemaining === 0) {
                         var gameOver = "Game Over! Press the enter key to try again!";
@@ -170,7 +162,7 @@ function startGame() {
 
                         // Enter key resets game
                         guessesMade = [];
-                        document.onkeyup = function(event) {
+                        document.onkeyup = function (event) {
                             if (event.which == 13 || event.keyCode == 13) {
                                 restartGame();
                                 startGame();
@@ -193,11 +185,10 @@ function startGame() {
             guessesRemaining = 10;
             newWord();
             document.getElementById("listedLetters").innerHTML = "<li id='guess'> </li>";
-            var gameStart = "Type any letter to make a guess!";
+            var gameStart = "Enter letter to make a guess!";
             document.querySelector("#game").innerHTML = gameStart;
         }
 
-        //END OF DOCUMENT ON KEY UP EVENT
     }
 
 }
